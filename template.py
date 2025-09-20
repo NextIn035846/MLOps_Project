e54a0e23c0fbd28e7a5ff192d90f5d2f9cecc811
Thomas Genexa <thomas.patole@genexa.ai>
2025-09-20 17:58:13 +0530

    Folder Structure Update

---
+++ template.py
@@ -0,0 +1,60 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+package_name = "mongodb_connect"
+
+list_of_files = [
+  # ".github/workflows/.gitkeep", This Is use when You Want to Push Empty Directories
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   "src/components/__init__.py",
+   "src/components/data_ingestion.py",
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluator.py",
+   "src/pipeline/__init__.py",
+   "src/pipeline/training_pipeline.py",
+   "src/pipeline/prediction_pipeline.py",
+   "src/utils/__init__.py",
+   "src/utils/utils.py",
+   "src/logger/__init__.py",
+   "src/logger/logging.py",
+   "src/exceptions/__init__.py",
+   "src/exceptions/exceptions.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
